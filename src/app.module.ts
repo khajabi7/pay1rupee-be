51be3d613345609fb8e4d60dc32621ee01cc3d70
Siraj <sirajsabir7@gmail.com>
2025-06-10 13:36:43 +0530

    updated .env and Use explicit DB params to force IPv4

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,34 +4,24 @@ import { ConfigModule } from '@nestjs/config';
 import { PaymentModule } from './payment/payment.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      //logging: true,
-      logging:['error', 'query', 'schema'],
+      logging: ['error', 'query', 'schema'],
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      //host: process.env.DATABASE_HOST || 'localhost',
-      //port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-      //username: process.env.DATABASE_USER || 'postgres',
-      //password: process.env.DATABASE_PASSWORD || 'postgres',
-      //database: process.env.DATABASE_NAME || 'pay1rupee',
       autoLoadEntities: true,
-      synchronize: false, // Disable sync
-      //synchronize: process.env.NODE_ENV === 'production' ? false : true,
-      //ssl: process.env.PGSSLMODE === 'require' ? { rejectUnauthorized: false } : false,
+      synchronize: false, // Disable sync in production
       ssl: {
-        rejectUnauthorized: false, // Required for Supabase
+        rejectUnauthorized: false, // Bypass certificate validation
       },
       logger: 'advanced-console',
       extra: {
         connectionTimeoutMillis: 5000,
-        max: 10,
-        //family: process.env.NODE_PG_FORCE_IPV4 === 'true' ? 4 : undefined,
-        family: 4, // Force IPv4
+        max: 10, // Connection pool size
+        family: 4, // Force IPv4 for Render
       },
     }),
     PaymentModule,
@@ -42,17 +32,9 @@ import { AppService } from './app.service';
 export class AppModule {
   constructor() {
     console.log('DB Config:', {
-      //host: process.env.DATABASE_HOST,
-      //port: process.env.DATABASE_PORT,
       url: process.env.DATABASE_URL,
-      //user: process.env.DATABASE_USER,
-      //db: process.env.DATABASE_NAME,
       ssl: process.env.PGSSLMODE,
-      
-      //family: process.env.NODE_PG_FORCE_IPV4 === 'true' ? 4 : undefined,
-
       family: 4,
     });
   }
 }
-
